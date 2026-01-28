c83c968686293ec55f61041872b18d8835debb1a
bogosj <bogosj@users.noreply.github.com>
2026-01-28 22:13:42 +0000

    Deploying to site from @ bogosj/superbowl-box-tracker@a0ed8c6ad1800e6995217490257d6f826ddd0554 🚀

--- README.md
+++
@@ -1,1 +0,0 @@
-# superbowl-box-tracker
---
+++ registerSW.js
@@ -0,0 +1,1 @@
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/superbowl-box-tracker/sw.js', { scope: '/superbowl-box-tracker/' })})}
